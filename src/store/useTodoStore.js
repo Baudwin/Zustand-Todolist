0f51887ca743bff0b2075d38e7d7d66eaa71e8df
Titah Baudwin <baudwin12@gmail.com>
2024-03-05 00:30:03 +0100

    added some styling and edit feature

--- src/store/useTodoStore.js
+++ src/store/useTodoStore.js
@@ -4,7 +4,7 @@ import { create } from "zustand";
 export const useTodoStore = create((set, get)=>({
     tasks : JSON.parse(localStorage.getItem('tasks')) || [],
     addTask : (newTask)=>{
-        const updatedTasks = [...get().tasks, newTask]
+        const updatedTasks = [...get().tasks, newTask].slice(0).reverse()
         
         localStorage.setItem('tasks', JSON.stringify(updatedTasks))
         set({tasks: updatedTasks})
@@ -15,5 +15,21 @@ export const useTodoStore = create((set, get)=>({
         set({tasks: updatedTasks})
         localStorage.setItem("tasks", JSON.stringify(updatedTasks))
         
+    }, 
+    updateTask : (id, updatedTask)=>{
+        const existingTasks = get().tasks
+       const updated = existingTasks.map((task,i)=>{
+            if (id === i) {
+              return  ({...task,content:updatedTask.content, status:updatedTask.status, date:updatedTask.date});
+            }
+            return task
+        })
+        set({tasks:updated})
+        localStorage.setItem('tasks', JSON.stringify(updated))
     }
+
+
+
+
+
 }))
