0e9883685b5b0ef148a7eb9f9395cb04158423ac
Titah Baudwin <baudwin12@gmail.com>
2024-03-05 00:44:17 +0100

    latest commit

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -73,18 +73,18 @@ export default function TodoContainer() {
     }
 
   return (
-    <div className='p-5  bg-[url("/images/bg2.jpg")] h-screen'>
+    <div className='p-5  bg-[url("/images/bg2.jpg")] min-h-screen'>
 
     <div  className="rounded-md shadow-xl lg:w-1/2 xl:w-1/2 md:w-1/2 m-auto space-y-5 p-2 mt-10 border  font-[popins]  ">
 
         <h1 className='font-semibold text-3xl sticky top-0 text-center text-black font-mono '><span className='underline'>TO</span> <span className='underline'>DO</span>  LIST</h1>
         <div className='space-y-2 p-2'>
-        <input  value={task.content} onChange={handleChange} className='border-b-2 rounded-lg shadow-sm w-full p-2.5 focus:outline-none' type="text" placeholder='what would you like to add? ' name="content" id="" />
+        <input  value={task.content} onChange={handleChange} className='border-b-2 rounded-lg shadow-sm w-full p-4 focus:outline-none' type="text" placeholder='what would you like to add? ' name="content" id="" />
         <input hidden value={task.status} className='' onChange={handleChange}  type="text" name="status" id="" />
         <input hidden value={task.date} className='' onChange={handleChange}  type="text" name="date" id="" />
 
-        <button  onClick={editing ? update : handleClick} className='px-7 py-1 rounded-lg shadow-md border border-purple-500 hover:text-white hover:bg-purple-400'> {editing ? 'Update' : "Add"}</button>
-       {editing ?<button onClick={cancelEdit} className='px-6 py-1 rounded-lg shadow-md border border-red-500 hover:text-white ml-2 hover:bg-red-400'> Cancel</button>:null } 
+        <button  onClick={editing ? update : handleClick} className='px-9 py-2 rounded-lg shadow-md border border-purple-500 hover:text-white hover:bg-purple-400'> {editing ? 'Update' : "Add"}</button>
+       {editing ?<button onClick={cancelEdit} className='px-9 py-2 rounded-lg shadow-md border border-red-500 hover:text-white ml-2 hover:bg-red-400'> Cancel</button>:null } 
         </div>
 
     <div style={{height:'400px'}} className='no-scrollbar space-y-1.5 overflow-y-scroll'>
@@ -94,14 +94,14 @@ export default function TodoContainer() {
         <span className='space-x-2' >
 
 <div onClick={()=>editTask(i,todo.content)}  className=' inline-block'>
-    <svg  xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+    <svg  xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
         <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
         </svg>
 </div>
    
 <div onClick={()=>deleteTask(i)} className='inline-block'>
-<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" className={`bi bi-trash3 cursor-pointer text-red-500 ${editing?'hidden':null}`} viewBox="0 0 16 16">
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className={`bi bi-trash3 cursor-pointer text-red-500 ${editing?'hidden':null}`} viewBox="0 0 16 16">
         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
         </svg>
 </div>
